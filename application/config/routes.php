43ce610465c9e1fb1df36d4051a53d1240dd5a77
MSI-Judy Ann Macutay <39181300+jannmacutay@users.noreply.github.com>
2023-02-13 23:02:35 +0800

    Not really important

--- application/config/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-defined('BASEPATH') or exit('No direct script access allowed');
-
-$route['default_controller'] = 'main/view';
-$route['(:any)'] = 'main/view/$1';
-$route['404_override'] = 'errors/html/error_404';
-$route['translate_uri_dashes'] = FALSE;
